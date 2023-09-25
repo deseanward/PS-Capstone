3827d3f29815396ad48599d4e5219515182b8123
De Sean Ward <d.ward.chi@gmail.com>
2023-09-25 13:39:59 -0500

    Testing fix for upload widget in production

--- src/components/upload-widget/upload-widget.component.jsx
+++ src/components/upload-widget/upload-widget.component.jsx
@@ -4,37 +4,32 @@ import { useDispatch } from "react-redux";
 import { setMedia } from "../../app/features/media/mediaSlice";
 
 const UploadWidget = ({ children, name, ...otherProps }) => {
-  const cloudinaryRef = useRef();
-  const widgetRef = useRef();
   const dispatch = useDispatch();
 
   const { className } = otherProps;
 
-  useEffect(() => {
-    cloudinaryRef.current = window.cloudinary;
-    widgetRef.current = cloudinaryRef.current.createUploadWidget(
-      {
-        cloudName: "dhcvpjhlc",
-        uploadPreset: "qs5htrib",
-      },
-      function (error, result) {
-        if (result.info.secure_url) {
-          console.log("GOT THE MEDIA: ", JSON.stringify(result.info.secure_url));
-          dispatch(
-            setMedia({
-              name: name,
-              url: result.info.secure_url,
-            })
-          );
-        }
-
-        if (error) console.log(error);
+  const widgetRef = window.cloudinary.createUploadWidget(
+    {
+      cloudName: "dhcvpjhlc",
+      uploadPreset: "qs5htrib",
+    },
+    function (error, result) {
+      if (result.info.secure_url) {
+        console.log("GOT THE MEDIA: ", JSON.stringify(result.info.secure_url));
+        dispatch(
+          setMedia({
+            name: name,
+            url: JSON.stringify(result.info.secure_url),
+          })
+        );
       }
-    );
-  }, [dispatch, name]);
+
+      if (error) console.log(error);
+    }
+  );
 
   return (
-    <div className={className} onClick={() => widgetRef.current.open()}>
+    <div className={className} onClick={() => widgetRef.open()}>
       {children}
     </div>
   );
